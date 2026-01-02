ad796678fcaa3ad7f88283ff7b224a244e11bcc9
Nirajkadam07 <64134180+Nirajkadam07@users.noreply.github.com>
2026-01-02 23:22:47 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,17 @@
-import { createRoot } from "react-dom/client"
+import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"))
+
 root.render(
-    <h1>Hello from React!</h1>
+    <main>
+        <img src="react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
 )
